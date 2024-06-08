afc218d1321a7f0fa21cde7124cbb850307b52ee
Bhavya Sai Keerthi Thammineni <119335955+BhavyaSaiKeerthiThammineni14@users.noreply.github.com>
2024-06-09 05:29:13 +0530

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, HTTPException, Query
-from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 import os
 import json
+from components import *
 from groq import Groq
+
 
 app = FastAPI()
 
@@ -15,62 +17,82 @@ if api_key is None:
 
 client = Groq(api_key=api_key)
 
-message = [
-    {
-        "role": "system",
-        "content": '''Imagine you are a children's storybook author. Create an interactive story where the main character faces several key decision points. At each of these points, offer a set of possible actions the main character can take. I will choose an action from the provided options to continue the story. Additionally, for each scenario, provide a text prompt to generate an AI image to visualize the scene for the child.
-
-Use the following JSON format for your output:
-
-{
-  "theme" : [
-  {"choice":"Option 1 text"}
-  {"choice":"Option 2 text"}
-  {"choice":"Option 3 text"}
-  {"choice":"Option 4 text"}
-  ]
-  "narration": "Story text here",
-  "question": "Question text here",
-  "options": [
-    { "choice": "Option 1 text", "nextStory": "Continuation of the story based on option 1" },
-    { "choice": "Option 2 text", "nextStory": "Continuation of the story based on option 2" },
-    { "choice": "Option 3 text", "nextStory": "Continuation of the story based on option 3" },
-    { "choice": "Option 4 text", "nextStory": "Continuation of the story based on option 4" }
-  ],
-  "image_prompt": "Text to generate the image"
-}
-
-Guidelines:
-
-    Question: Frame a question based on the current scenario in the story.
-    Options: Provide 4 different main points with minimal words as choices the main character can take at each key decision point, with each choice leading to a different continuation of the story.
-    Next Story: Write a continuation of the story for each option.
-    Image Prompt: Create a descriptive text that captures the scene in the story, which can be used to generate an AI image.
-    Ending: The story should always end meaningfully between 19 to 25 responses from the user with a moral for children.
-    Strict JSON Output: Return only the JSON output without any additional text or explanations.''',
-    },
-    {
-        "role": "user",
-        "content": "start",
-    },
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
-@app.get("/")
-def root():
+
+
+
+
+
+
+
+
+
+interaction = 0
+message = []
+options = []
+
+
+
+
+
+@app.post("/themes")
+def root(theme : str = Query(...)):
+    global message, options, interaction
+    interaction = 0
+    message = []
+    message.append({"role" : "user", "content" : f"generate me with {theme} of the story"})
     try:
         completion = client.chat.completions.create(
             model="llama3-8b-8192",
-            messages=message,
+            messages=start+message,
             temperature=1,
-            max_tokens=1220,
+            max_tokens=1024,
+            top_p=1,
+            stream=False,
+            response_format={"type": "json_object"},
+            stop=None,
+        )
+        role = completion.choices[0].message.role
+        content = completion.choices[0].message.content
+        message.append({"role": role, "content": content})
+
+        data = json.loads(content)
+
+        narration = data['narration']
+        question = data['question']
+        options = data['options']
+        print(options)
+        image_prompt = data['image_prompt']
+                
+
+        return {"narration": narration, "question": question, "options": options, "image_prompt": image_prompt}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+
+"""def try_again():
+    global message, options
+    message.pop()
+    try:
+        completion = client.chat.completions.create(
+            model="llama3-8b-8192",
+            messages=message[-4:],
+            temperature=1,
+            max_tokens=1024,
             top_p=1,
             stream=False,
             response_format={"type": "json_object"},
@@ -90,17 +112,61 @@ def root():
         return {"narration": narration, "question": question, "options": options, "image_prompt": image_prompt}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    """
 
-@app.post("/options")
-def set_option(option: str = Query(...)):
-    message.append({"role": "user", "content": option})
-    
+
+
+
+def end_story(option):
+    global message, interaction
+    print(interaction+1)
+    interaction=0
+    message.append({"role": "user", "content": option })
     try:
         completion = client.chat.completions.create(
             model="llama3-8b-8192",
-            messages=message,
+            messages=end+message[-4:],
             temperature=1,
-            max_tokens=1220,
+            max_tokens=1024,
+            top_p=1,
+            stream=False,
+            response_format={"type": "json_object"},
+            stop=None,
+        )
+        role = completion.choices[0].message.role
+        content = completion.choices[0].message.content
+        message.append({"role": role, "content": content})
+
+        data = json.loads(content)
+
+        narration = data['narration']
+        question = data['question']
+        options = data['options']
+        image_prompt = data['image_prompt']
+
+        return {"narration": narration, "question": question, "options": options, "image_prompt": image_prompt}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+
+
+
+@app.post("/options")
+def set_option(option: str = Query(...)):
+    global interaction, message
+    interaction += 1
+    if interaction >= 12:
+        return end_story(option)
+    
+    message.append({"role" : "user", "content":option})
+    print(interaction)
+    try:
+        completion = client.chat.completions.create(
+            model="llama3-8b-8192",
+            messages=start+message[-4:],  # Use the last 4 messages to maintain context
+            temperature=1,
+            max_tokens=1024,
             top_p=1,
             stream=False,
             response_format={"type": "json_object"},
